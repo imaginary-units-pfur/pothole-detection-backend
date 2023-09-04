9f37b85a8e4f9eb4e125a7378579e034c8bfe34d
Grigorii Matiukhin <grigory36m@gmail.com>
2023-09-04 14:14:52 +0300

    tracing

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,23 @@
 use axum::{routing::get, Router};
 mod routes;
+use tower_http::trace::TraceLayer;
+
+#[derive(Clone)]
+struct ServerCtx {
+    db_client: redis::Client,
+}
 
 pub async fn run(db_url: &str) {
-    let _client = redis::Client::open(db_url).unwrap();
-    let app = Router::new().route("/", get(routes::root));
-    axum::Server::bind(&"0.0.0.0:80".parse().unwrap())
+    tracing_subscriber::fmt::init();
+
+    let client = redis::Client::open(db_url).unwrap();
+    let ctx = ServerCtx { db_client: client };
+    let app = Router::new()
+        .route("/", get(routes::root))
+        .with_state(ctx)
+        .layer(TraceLayer::new_for_http());
+
+    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use std::env;
 
 #[tokio::main]
 async fn main() {
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     let db_url = env::var("REDIS_URL").expect("`REDIS_URL` is not set.");
     run(&db_url).await;
 }
